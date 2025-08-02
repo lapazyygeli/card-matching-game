e9e73f5c8e6bfa45afce0259d689b8065cf683ab
lapazyygeli <137160486+lapazyygeli@users.noreply.github.com>
2025-08-02 19:23:29 +0300

    Update Gameboard component

--- frontend/src/components/Gameboard.tsx
+++ frontend/src/components/Gameboard.tsx
@@ -1,20 +1,31 @@
+import { Card } from './Card'
+
+type ImageModuleNamespaceObject = { default: string }
+const avatarModules = import.meta.glob<ImageModuleNamespaceObject>(
+  // The <Type> syntax tells the compiler
+  // what the types of the object’s values are.
+  '../assets/cards/*.png',
+  { eager: true }
+)
+const avatars = Object.values(avatarModules).map(
+  (modNamespaceObj) => modNamespaceObj.default
+)
+
 export function Gameboard() {
   return (
     <div>
-      <div className="flex justify-center pt-8">
+      <div className="flex justify-center py-8">
         <div className="w-full max-w-2xl p-4 bg-white">
           <div className="flex gap-4 mr-2 justify-end pb-6 text-[0.8rem]">
             <p>Attempts left:</p>
             <p>Elapsed time:</p>
           </div>
-          <div className="grid justify-center grid-cols-[repeat(2,80px)] sm:grid-cols-[repeat(4,90px)]">
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 12, 13, 14, 15, 16].map(
-              (card, i) => (
-                <div className="aspect-square" key={i}>
-                  {card}
-                </div>
-              )
-            )}
+          <div className="grid gap-4 justify-center grid-cols-[repeat(2,80px)] sm:grid-cols-[repeat(4,90px)]">
+            {avatars.map((avatar, i) => (
+              <div className="aspect-square" key={i}>
+                <Card avatar={avatar} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
